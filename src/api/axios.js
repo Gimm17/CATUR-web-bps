4b27173bb28db30cf30a08a8cfb80e4c734de06d
Gimm17 <agimpratamazte@gmail.com>
2026-09-09 14:27:22 +0800

    chore: allow configurable API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://catur.bpssulteng.id/api',
+  baseURL: (import.meta.env.VITE_API_BASE_URL || 'https://catur.bpssulteng.id/api')
+    .replace(/\/+$/, ''),
 });
 
 api.interceptors.request.use((config) => {
